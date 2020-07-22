fae80e3fab1fa52e10920dd51da287add0a90a55
ZSF13027765337 <1607944289@qq.com>
2020-07-22 17:10:05 +0800

    整合druid

---
+++ src/main/java/com/boot/myboot/config/DruidStatFilter.java
@@ -0,0 +1,18 @@
+package com.boot.myboot.config;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * 配置druid过滤规则
+ * @author zsf
+ *
+ */
+@WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
+        initParams={
+                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+        })
+public class DruidStatFilter extends WebStatFilter {
+}
